a0201f175f26b68e63c5f5f7afb13949482cc3d5
cahnom@gmail.com <cahnom@gmail.com>
2015-03-15 22:30:39 +0700

    Basic React.js hello world application

---
+++ ReactBundling/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReactBundling.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetName()
+        {
+            return Json(new { name = "Dunia dari sisi server" }, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ ReactBundling/Scripts/jsx/HaloDunia.jsx
@@ -0,0 +1,28 @@
+﻿var HaloDunia = React.createClass({
+ 
+getInitialState: function() {
+    return { name:''};
+},
+componentDidMount: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function(data) {
+        this.setState(data);
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+},
+    render : function(){
+        return (
+        <div> Hello {this.state.name} </div>
+        );
+    }
+});
+ 
+React.render(
+    <HaloDunia url="/home/getname" />,
+    document.getElementById('container')
+);
