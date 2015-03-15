8969011fc5e34a023bc312ef064cc52aad4c22df
cahnom@gmail.com <cahnom@gmail.com>
2015-03-15 23:40:45 +0700

    Configure Bundling and Minification

---
+++ ReactBundling/App_Start/BundleConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Optimization;
+using System.Web.Optimization.React;
+
+namespace ReactBundling
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new JsxBundle("~/bundles/react-halodunia")
+                .Include("~/Scripts/react/react-{version}.js")
+                .Include("~/Scripts/jsx/HaloDunia.jsx"));
+        }
+    }
+}
